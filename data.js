7c8037623a4fc080dbf44ae892cff809f09d884e
seamori123 <seamorrri@gmail.com>
2025-12-30 21:27:01 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,16 @@
+const DATA = {
+  subjects: [
+    {
+      id: "pedagogika",
+      title: "Педагогика",
+      projects: [
+        {
+          id: "prof-deformaciya",
+          title: "1.1 2.6. Профессиональные деформации педагога",
+          presentationPdf: "assets/pdf/prof-deformaciya-pptx.pdf",
+          textPdf: "assets/pdf/prof-deformaciya-docs.pdf"
+        }
+      ]
+    }
+  ]
+};
